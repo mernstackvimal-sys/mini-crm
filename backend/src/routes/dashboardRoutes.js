4d68339dc37dc5382065d442f5b15690c5ae34b4
mernstackvimal-sys <mernstack.vimal@gmail.com>
2026-08-28 15:37:59 +0530

    Remove existing code

--- backend/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardStats } = require('../controllers/dashboardController');
-const { protect } = require('../middleware/auth');
-
-router.get('/', protect, getDashboardStats);
-
-module.exports = router;
